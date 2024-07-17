8f4d7f124bdb6bd5643fba262894bb30bdb761de
meetqy <meetqy@icloud.com>
2024-07-17 13:31:40 +0800

    trpc

--- app/[locale]/@modal/ai-slug-generator/page.tsx
+++ app/[locale]/@modal/ai-slug-generator/page.tsx
@@ -1,14 +1,17 @@
+"use client";
+
 import { ModalPage } from "@/components/modal-page";
+import { api } from "@/trpc/react";
 import { useTranslations } from "next-intl";
 
 export default function App() {
   const t = useTranslations("AiSlugGenerator");
+  const { data } = api.aiSlugGenerator.hello.useQuery({ text: "123" });
 
   return (
     <ModalPage
       header={
         <>
-          {" "}
           <h1 className="text-large">{t("title")}</h1>
           <p className="font-normal text-default-500 text-medium">
             {t("description")}
@@ -16,7 +19,7 @@ export default function App() {
         </>
       }
     >
-      123
+      {data?.greeting}
     </ModalPage>
   );
 }
--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -11,6 +11,7 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { Toaster } from "react-hot-toast";
 import { Dock } from "../dock";
+import { TRPCReactProvider } from "@/trpc/react";
 
 export const metadata: Metadata = {
   title: {
@@ -55,15 +56,19 @@ export default async function RootLayout({
           backgroundImage: `url(https://images.unsplash.com/photo-1718348636186-5620c97d4c1f?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
         }}
       >
-        <NextIntlClientProvider messages={messages}>
-          <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
-            {children}
-            {modal}
-            <Screen />
-            <Toaster />
-            <Dock />
-          </Providers>
-        </NextIntlClientProvider>
+        <TRPCReactProvider>
+          <NextIntlClientProvider messages={messages}>
+            <Providers
+              themeProps={{ attribute: "class", defaultTheme: "light" }}
+            >
+              {children}
+              {modal}
+              <Screen />
+              <Toaster />
+              <Dock />
+            </Providers>
+          </NextIntlClientProvider>
+        </TRPCReactProvider>
       </body>
     </html>
   );
