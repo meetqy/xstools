8bfbacecb57b1b9945df8427216e1a0c5c6365a1
meetqy <meetqy@icloud.com>
2024-07-18 15:59:26 +0800

    login

--- app/dock.tsx
+++ app/dock.tsx
@@ -2,14 +2,15 @@
 
 import { useRouter } from "@/navigation";
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
-import { Card, Image } from "@nextui-org/react";
+import { Icon } from "@iconify/react";
+import { Button, Card, Image } from "@nextui-org/react";
 
 export const Dock = () => {
   const router = useRouter();
 
   return (
     <header className="h-16 fixed bottom-2 left-0 w-full md:px-0 px-4">
-      <div className="mx-auto max-w-xl bg-background/50 backdrop-blur-sm h-full shadow-medium rounded-medium flex items-center p-2 gap-2">
+      <div className="mx-auto justify-between max-w-xl bg-background/50 backdrop-blur-sm h-full shadow-medium rounded-medium flex items-center p-2 gap-2">
         <Card
           isPressable
           className="w-12 h-12 p-0 bg-transparent"
@@ -17,12 +18,22 @@ export const Dock = () => {
         >
           <Image src="/icons/about-xstools.svg" className="w-full h-full" />
         </Card>
-        <div>
+        <div className="flex justify-center items-center">
           <SignedOut>
-            <SignInButton />
+            <SignInButton>
+              <Card isPressable className="w-12 h-12 p-0 bg-transparent">
+                <Image src="/icons/profile.svg" className="w-full h-full" />
+              </Card>
+            </SignInButton>
           </SignedOut>
           <SignedIn>
-            <UserButton />
+            <UserButton
+              appearance={{
+                elements: {
+                  userButtonAvatarBox: "w-12 h-12",
+                },
+              }}
+            />
           </SignedIn>
         </div>
       </div>
