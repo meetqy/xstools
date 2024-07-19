579c84cc5608ddbaaa3afb6f27edfe534797ab0c
meetqy <meetqy@icloud.com>
2024-07-19 10:14:46 +0800

    length unit

--- app/[locale]/@modal/length-unit-converter/page.tsx
+++ app/[locale]/@modal/length-unit-converter/page.tsx
@@ -3,39 +3,43 @@
 import { ModalPage } from "@/components/modal-page";
 import { Input } from "@nextui-org/react";
 import { useState } from "react";
+import { round } from "mathjs";
+import { useTranslations } from "next-intl";
 
 const _metric = ["km", "m", "dm", "cm", "mm", "um", "nm", "pm"];
 const _british = ["nmi", "fm", "ft", "in", "mi", "fur", "yd", "mil"];
 const _chinese = ["gongli", "zhang", "chi", "cun", "fen", "li", "hao"];
 
 export default function Page() {
+  const t = useTranslations("LengthUnitConverter");
+
   // 长度单位
   const [unit, setUnit] = useState({
-    km: { value: "", name: "千米", ratio: 1e-3 },
-    m: { value: "", name: "米", ratio: 1 },
-    dm: { value: "", name: "分米", ratio: 1e1 },
-    cm: { value: "", name: "厘米", ratio: 1e2 },
-    mm: { value: "", name: "毫米", ratio: 1e3 },
-    um: { value: "", name: "微米", ratio: 1e6 },
-    nm: { value: "", name: "纳米", ratio: 1e9 },
-    pm: { value: "", name: "皮米", ratio: 1e12 },
-    // 英制
-    nmi: { value: "", name: "海里", ratio: 1 / 1852 },
-    fm: { value: "", name: "英寻", ratio: 1 / 1.8288 },
-    ft: { value: "", name: "英尺", ratio: 1 / 0.3048 },
-    in: { value: "", name: "英寸", ratio: 1 / 0.0254 },
-    mi: { value: "", name: "英里", ratio: 1 / 1609.344 },
-    fur: { value: "", name: "弗隆", ratio: 1 / 201.168 },
-    yd: { value: "", name: "码", ratio: 1 / 0.9144 },
-    mil: { value: "", name: "密尔", ratio: 1 / 0.0000254 },
-    // 市制
-    gongli: { value: "", name: "公里", ratio: 1 / 500 },
-    zhang: { value: "", name: "丈", ratio: 1 / 10 },
-    chi: { value: "", name: "尺", ratio: 1 },
-    cun: { value: "", name: "寸", ratio: 10 },
-    fen: { value: "", name: "分", ratio: 100 },
-    li: { value: "", name: "厘", ratio: 1000 },
-    hao: { value: "", name: "毫", ratio: 10000 },
+    km: { value: "", name: t("kilometre"), ratio: 1e-3 },
+    m: { value: "", name: t("meter"), ratio: 1 },
+    dm: { value: "", name: t("decimeter"), ratio: 1e1 },
+    cm: { value: "", name: t("centimeter"), ratio: 1e2 },
+    mm: { value: "", name: t("millimeter"), ratio: 1e3 },
+    um: { value: "", name: t("micrometer"), ratio: 1e6 },
+    nm: { value: "", name: t("nanometer"), ratio: 1e9 },
+    pm: { value: "", name: t("picometer"), ratio: 1e12 },
+    // British
+    nmi: { value: "", name: t("nautical-mile"), ratio: 1 / 1852 },
+    fm: { value: "", name: t("fathom"), ratio: 1 / 1.8288 },
+    ft: { value: "", name: t("foot"), ratio: 1 / 0.3048 },
+    in: { value: "", name: t("inch"), ratio: 1 / 0.0254 },
+    mi: { value: "", name: t("mile"), ratio: 1 / 1609.344 },
+    fur: { value: "", name: t("furlong"), ratio: 1 / 201.168 },
+    yd: { value: "", name: t("yard"), ratio: 1 / 0.9144 },
+    mil: { value: "", name: t("mil"), ratio: 1 / 0.0000254 },
+    // Chinese
+    gongli: { value: "", name: t("gong-li"), ratio: 1e-3 },
+    zhang: { value: "", name: t("zhang"), ratio: 1 / 10 },
+    chi: { value: "", name: t("chi"), ratio: 1 },
+    cun: { value: "", name: t("cun"), ratio: 10 },
+    fen: { value: "", name: t("fen"), ratio: 100 },
+    li: { value: "", name: t("li"), ratio: 1000 },
+    hao: { value: "", name: t("hao"), ratio: 10000 },
   });
 
   type Unit = keyof typeof unit;
@@ -81,7 +85,7 @@ export default function Page() {
         if (k === key) return;
 
         const item = newUnit[k as Unit];
-        item.value = (num / prev[key].ratio) * item.ratio + "";
+        item.value = round((num / prev[key].ratio) * item.ratio, 6).toString();
       });
 
       return newUnit;
@@ -92,15 +96,15 @@ export default function Page() {
     <ModalPage
       header={
         <>
-          <h1 className="text-large">Length Unit Converter</h1>
+          <h1 className="text-large">{t("title")}</h1>
           <p className="font-normal text-default-500 text-medium">
-            Quickly Convert Between Different Units of Length
+            {t("description")}
           </p>
         </>
       }
     >
       <div className="space-y-4">
-        <p className="border-b text-primary">公制</p>
+        <p className="border-b text-primary">{t("metric-system")}</p>
         <section className="grid grid-cols-4 gap-4">
           {Object.keys(metric).map((key) => {
             const item = unit[key as Unit];
@@ -120,7 +124,7 @@ export default function Page() {
           })}
         </section>
 
-        <p className="border-b text-primary">英制</p>
+        <p className="border-b text-primary">{t("british-system")}</p>
         <section className="grid grid-cols-4 gap-4">
           {Object.keys(british).map((key) => {
             const item = british[key as Unit];
@@ -140,7 +144,9 @@ export default function Page() {
           })}
         </section>
 
-        <p className="border-b text-primary">市制</p>
+        <p className="border-b text-primary">
+          {t("the-chinese-system-of-weights-and-measures")}
+        </p>
         <section className="grid grid-cols-4 gap-4">
           {Object.keys(chinese).map((key) => {
             const item = chinese[key as Unit];
