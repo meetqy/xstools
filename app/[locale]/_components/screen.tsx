42bf360393e57dc9a4725907cec7403e59f4acc6
meetqy <meetqy@icloud.com>
2024-07-21 00:46:18 +0800

    ui

--- app/[locale]/_components/screen.tsx
+++ app/[locale]/_components/screen.tsx
@@ -20,19 +20,19 @@ const CardItem = ({
       radius="md"
       shadow="none"
       isPressable
-      className="aspect-square relative w-full md:w-24 z-10 bg-transparent select-none p-1"
+      className="aspect-square relative w-full z-10 bg-transparent select-none p-1"
       onPress={() => router.push(url)}
     >
       <CardHeader className="p-0">
         <Image
           src={icon}
           alt={icon.split("-").join(" ")}
-          className="w-10/12 aspect-square m-auto"
+          className="w-10/12 md:w-8/12 aspect-square m-auto"
           draggable={false}
         />
       </CardHeader>
       <CardFooter className="justify-center px-2">
-        <p className="capitalize truncate text-xs md:text-small text-default-50">
+        <p className="capitalize truncate text-xs md:text-small text-default-800">
           {title}
         </p>
       </CardFooter>
@@ -52,7 +52,7 @@ export function Screen() {
   ];
 
   return (
-    <div className="grid grid-cols-4 md:flex flex-wrap p-4 md:gap-4 gap-x-4 gap-y-6">
+    <div className="grid grid-cols-4 p-4 gap-x-4 gap-y-6 md:grid-cols-12">
       {apps.map((item) => {
         return (
           <CardItem
