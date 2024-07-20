8ebd79fed4833908c0a16c2b8ec9f24c4aabe3c2
meetqy <meetqy@icloud.com>
2024-07-20 17:34:56 +0800

    fixbug

--- app/[locale]/@modal/check-webcam/page.tsx
+++ app/[locale]/@modal/check-webcam/page.tsx
@@ -4,37 +4,51 @@ import { ModalPage } from "@/components/modal-page";
 import { Button, Select, SelectItem } from "@nextui-org/react";
 import { useTranslations } from "next-intl";
 import { useEffect, useRef, useState } from "react";
-import toast from "react-hot-toast";
 
 export default function App() {
   const t = useTranslations("CheckWebcam");
+
+  // 用来做选项的
   const [webcam, setWebcam] = useState<MediaDeviceInfo[]>([]);
   const videoRef = useRef<HTMLVideoElement>(null);
   const [stream, setStream] = useState<MediaStream>();
   const [selectedWebcam, setSelectedWebcam] = useState<string>();
 
+  const getWebcam = async () => {
+    navigator.mediaDevices.enumerateDevices().then((devices) => {
+      setWebcam(devices.filter((device) => device.kind === "videoinput"));
+    });
+  };
+
   useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({
-        video: true,
-      })
-      .then((_stream) => {
-        navigator.mediaDevices.enumerateDevices().then((devices) => {
-          setWebcam(devices.filter((device) => device.kind === "videoinput"));
-        });
-      })
-      .catch(() => {
-        toast.error("Please allow the camera access to continue.");
-      });
+    getWebcam();
   }, []);
+
+  // 判断是否有权限
+  const isPermission = webcam.every((item) => item.deviceId);
+
+  // 获取视频流 播放摄像头画面
+  const getStream = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: {
+          width: videoRef.current?.clientWidth,
+          height: videoRef.current?.clientHeight,
+        },
+      });
+
+      setStream(stream);
+      return stream;
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   const getUserMedia = async () => {
     try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: { deviceId: selectedWebcam },
-      });
+      const stream = await getStream();
 
-      if (videoRef.current) {
+      if (videoRef.current && stream) {
         videoRef.current.srcObject = stream;
         videoRef.current.play();
         setStream(stream);
@@ -64,41 +78,67 @@ export default function App() {
         </>
       }
     >
-      <div className="flex md:flex-row flex-col-reverse gap-4 max-w-5xl w-full mx-auto">
-        <video
-          className="flex-1 aspect-video rounded-medium"
-          ref={videoRef}
-          style={{ background: "#000 url(/check-webcam/noise.gif)" }}
-        />
-        <div className="flex flex-col gap-4 md:w-3/12 w-full items-center md:items-start">
-          <Select
-            label="Select a webcam"
-            value={selectedWebcam}
-            onChange={(e) => setSelectedWebcam(e.target.value)}
-            size="sm"
-            variant="bordered"
-            fullWidth
-          >
-            {webcam.map((item) => (
-              <SelectItem key={item.deviceId}>
-                {item.label.replace(/\((.*?)+/g, "")}
-              </SelectItem>
-            ))}
-          </Select>
-          {stream ? (
-            <Button onPress={stopStream} color="warning" fullWidth>
-              {t("stop-webcam")}
-            </Button>
-          ) : (
-            <Button
-              onPress={getUserMedia}
-              color="primary"
-              isDisabled={!selectedWebcam}
+      <div className="flex flex-col items-start relative">
+        {!isPermission && (
+          <div className="absolute top-0 left-0 bg-danger w-full text-danger-foreground shadow-large p-4 rounded-medium z-50">
+            <p>{t("tips")}</p>
+            <p className="text-right">
+              <Button
+                size="sm"
+                className="mt-2"
+                variant="solid"
+                color="warning"
+                onPress={async () => {
+                  const stream = await getStream();
+                  if (stream) {
+                    stream.getTracks().forEach((track) => track.stop());
+                    setStream(undefined);
+                    getWebcam();
+                  }
+                }}
+              >
+                {t("tips-btn")}
+              </Button>
+            </p>
+          </div>
+        )}
+        <div className="flex md:flex-row flex-col-reverse gap-4 max-w-5xl w-full mx-auto">
+          <video
+            className="flex-1 aspect-video rounded-medium"
+            ref={videoRef}
+            style={{ background: "#000 url(/check-webcam/noise.gif)" }}
+          />
+          <div className="flex flex-col gap-4 md:w-3/12 w-full items-center md:items-start">
+            <Select
+              label={t("select-a-webcam")}
+              aria-label={t("select-a-webcam")}
+              value={selectedWebcam}
+              onChange={(e) => setSelectedWebcam(e.target.value)}
+              size="sm"
+              variant="bordered"
               fullWidth
             >
-              {t("check-my-webcam")}
-            </Button>
-          )}
+              {webcam.map((item) => (
+                <SelectItem key={item.deviceId}>
+                  {item.label.replace(/\((.*?)+/g, "")}
+                </SelectItem>
+              ))}
+            </Select>
+            {stream ? (
+              <Button onPress={stopStream} color="warning" fullWidth>
+                {t("stop-webcam")}
+              </Button>
+            ) : (
+              <Button
+                onPress={getUserMedia}
+                color="primary"
+                isDisabled={!selectedWebcam}
+                fullWidth
+              >
+                {t("check-my-webcam")}
+              </Button>
+            )}
+          </div>
         </div>
       </div>
     </ModalPage>
